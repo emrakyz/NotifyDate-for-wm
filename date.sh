78a47fa670428938664bdb07917e7746beeac628
Emre AKYÜZ <emreakyuz_2@hotmail.com>
2024-05-28 14:16:18 +0300

    Improve && Simplify

--- date.sh
+++ date.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-
-D=$(date  +%d-%m-%y)
-T=$(date +%H:%M)
-notify-send -a "your wm" " $D  "" $T"
-#  - the icon in the Nerd Font
-# To make the file executable, write: chmod +x date.sh
+notify-send "Date" "  $(date '+%d-%m-%y    %H:%M')"
